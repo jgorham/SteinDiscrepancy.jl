13cf150078c9b58e6357ecc3bdd89f5f596436df
jackson <jacksongorham@gmail.com>
2017-05-20 16:22:15 -0700

    Add the kernel unittests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,7 +11,13 @@ function uniform_gradlogp(x::Array{Float64,1})
     zeros(size(x))
 end
 
-# Graph Stein discrepancy unbounded test
+function gaussian_gradlogp(x::Array{Float64,1})
+    -x
+end
+
+imqkernel = SteinInverseMultiquadricKernel()
+
+# Graph Stein discrepancy bounded test
 @testset "Graph discrepancy test" begin
     res = stein_discrepancy(points=UNIFORM_TESTDATA,
                             gradlogdensity=uniform_gradlogp,
@@ -22,4 +28,22 @@ end
 
     @test_approx_eq_eps res.objectivevalue[1] 0.308 1e-5
     @test_approx_eq_eps res.objectivevalue[2] 0.31 1e-5
+end
+
+# Kernel Stein discrepancy test
+@testset "Univariate Kernel discrepancy test" begin
+    res = stein_discrepancy(points=GAUSSIAN_TESTDATA[:,1],
+                            gradlogdensity=gaussian_gradlogp,
+                            method="kernel",
+                            kernel=imqkernel)
+
+    @test_approx_eq_eps res.discrepancy2 0.269298 1e-5
+end
+@testset "Multivariate Kernel discrepancy test" begin
+    res = stein_discrepancy(points=GAUSSIAN_TESTDATA,
+                            gradlogdensity=gaussian_gradlogp,
+                            method="kernel",
+                            kernel=imqkernel)
+
+    @test_approx_eq_eps res.discrepancy2 0.807566 1e-5
 end
